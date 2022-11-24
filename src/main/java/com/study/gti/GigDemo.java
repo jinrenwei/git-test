f7ef08c89ab6845dd544f0814cf9cecd0dcfbfef
Jrw <542002223@qq.com>
2022-11-24 17:03:23 +0800

    s

---
+++ src/main/java/com/study/gti/GigDemo.java
@@ -0,0 +1,11 @@
+package com.study.gti;
+
+/**
+ * @Author Jrw
+ * @Date 2022/11/24 17:01
+ */
+public class GigDemo {
+    public static void main(String[] args) {
+        System.out.println("test git");
+    }
+}
