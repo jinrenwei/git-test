8c6e699d9fbec57bdca3c33d004891d3abc517b0
Jrw <542002223@qq.com>
2022-11-24 17:08:38 +0800

    第二个分支第一次提交

--- src/main/java/com/study/gti/GigDemo.java
+++ src/main/java/com/study/gti/GigDemo.java
@@ -8,6 +8,8 @@ public class GigDemo {
     public static void main(String[] args) {
         System.out.println("test git");
         System.out.println("test git");
+        System.out.println("test git");
+
         //a
     }
 }
